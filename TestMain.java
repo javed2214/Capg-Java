f4c0cb353211648917a03ebf562edf9616cdcae3
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2020-02-11 10:56:13 +0530

    Update TestMain.java

--- TestMain.java
+++ TestMain.java
@@ -1,31 +1,43 @@
-import java.util.*;
+package com.ec2application.ec2application;
 
-public class TestMain {
-	
-	public static void main(String[] args){
-		
-		BusTicket busobj = getTicketDetails();
-	}
-	
-	public static BusTicket getTicketDetails(){
-		
-		BusTicket obj = new BusTicket();
-		Person p = new Person();
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Enter the pasenger name:");
-		obj.setPerson(p.setName(input.nextLine()));
-		
-		System.out.println("Gender:");
-		obj.getPerson().setGender(input.next().charAt(0));
-		
-		System.out.println("Enter the Ticket no:");
-		obj.setTicketNo(input.nextInt());
-		
-		System.out.println("Enter the Ticket Price: ");
-		obj.setTicketPrice(input.nextFloat());
-		
-		return obj;
-	}
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+
+public class EC2Stop
+{
+    private static final AWSCredentials AWS_CREDENTIALS;
+    
+    static {
+        // Your accesskey and secretkey
+        AWS_CREDENTIALS = new BasicAWSCredentials(
+                "id",
+                "secretkey"
+        );
+    }
+    public static void main(String[] args) {
+        
+        // Set up the amazon ec2 client
+        AmazonEC2 ec2Client = AmazonEC2ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(AWS_CREDENTIALS))
+                .withRegion(Regions.US_EAST_1)
+                .build();
+        
+        //Stop EC2 Instance
+        String instanecID = "i-0739c0ea4202ef40f";
+        StopInstancesRequest stopInstancesRequest = new StopInstancesRequest()
+                .withInstanceIds(instanecID);
+         
+        ec2Client.stopInstances(stopInstancesRequest)
+                .getStoppingInstances()
+                .get(0)
+                .getPreviousState()
+                .getName();
+        System.out.println("Stopped the Instnace with ID: "+instanecID);
+    }
 
 }
